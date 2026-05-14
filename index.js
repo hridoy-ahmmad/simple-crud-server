b39d32af3e813cc4b66d090f458115d923b86ef4
hridoy-ahmmad <hridoyp338y@gmail.com>
2026-05-15 02:06:22 +0600

    connect to mongoDB dataBASE and get collection

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT || 9000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-app.use(cors()) 
+app.use(cors())
 app.use(express.json())
 
 const uri = "mongodb+srv://simple_crud:Hp7615O4OTrWTfNH@cluster0.wb7lpvk.mongodb.net/?appName=Cluster0";
@@ -26,16 +26,25 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function run() {
+const run = async () => {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+        const database = client.db("simple-crud-DB");
+        const userCollection = database.collection('users')
+
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
